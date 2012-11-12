586a5892bfd45689acdf5ea36589e5cee19779df
stecco4 <stecco_4@yahoo.com>
2012-11-12 16:51:10 -0500

    fixed transperency

--- draw_polygons.cpp
+++ draw_polygons.cpp
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "header.h"
 
 #if defined(__MACOSX__) || defined(__APPLE__)
 #include <GLUT/glut.h>
@@ -10,30 +11,7 @@
 #include <GL/glut.h>
 #endif
 
-//#define DEFAULT_WIDTH 500
-//#define DEFAULT_HEIGHT 500
-//#define MAX_POINTS 6 //maximum points per polygon
-//#define MAX_POLYGONS 5
-//
-//typedef struct vertex3f {
-//    float x;
-//    float y;
-//    float z; // not particularly relevant for our purposes, but must be there
-//} vertex3f;
-//
-//typedef struct color4f {
-//    float r;
-//    float g;
-//    float b;
-//    float a; // alpha
-//} color4f;
-//
-//typedef struct polygon {
-//    int num_p; // number of points (cannot exceed MAX_POINTS)
-//    color4f color;
-//    vertex3f points[MAX_POINTS];
-//} polygon;
-//
+
 int num_polygons = 0;
 polygon polygons[MAX_POLYGONS];
 
@@ -46,8 +24,8 @@ void display(void){
 	
 	//draw a polygon	
     for(i=0;i<num_polygons;i++) {
-        glColor4f(polygons[i].color.r, polygons[i].color.g, polygons[i].color.b, polygons[i].color.g);
-        printf("%f %f %f %f\n", polygons[i].color.r, polygons[i].color.g, polygons[i].color.b, polygons[i].color.g);
+        glColor4f(polygons[i].color.r, polygons[i].color.g, polygons[i].color.b, polygons[i].color.a);
+        printf("%f %f %f %f\n", polygons[i].color.r, polygons[i].color.g, polygons[i].color.b, polygons[i].color.a);
         glBegin(GL_POLYGON);
             for(j=0;j<polygons[i].num_p;j++) {
                 glVertex3f(polygons[i].points[j].x, polygons[i].points[j].y, polygons[i].points[j].z);
@@ -56,14 +34,6 @@ void display(void){
         glEnd();
     }
     
-
-//	glColor3f(0.1, 0.3, 1.0);
-//	glBegin(GL_POLYGON);
-//		glVertex3f(0.20, 0.33, 0.0);
-//		glVertex3f(0.35, 0.67, 0.0);
-//		glVertex3f(0.65, 0.67, 0.0);
-//		glVertex3f(0.80, 0.33, 0.0);
-//	glEnd(); 
 
 	//process routines
 	glFlush();
@@ -79,7 +49,9 @@ void init(void){
 	//initial vieing values
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+	glOrtho(0.0f, 1.0f, 1.0f, 0.0f, -1.0f, 2.0f); // top left is (0,0) displays depths between -1 and 2
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_ONE,GL_ONE);
 	
 }
 
@@ -97,16 +69,16 @@ int main(int argc, char** argv){
         polygons[i].num_p = 3;
         polygons[i].points[0].x = (float)rand()/(float)RAND_MAX;
         polygons[i].points[0].y = (float)rand()/(float)RAND_MAX;
-        polygons[i].points[0].z = 0.0f;
+        polygons[i].points[0].z = (float)i/10.0f;
         polygons[i].points[1].x = (float)rand()/(float)RAND_MAX;
         polygons[i].points[1].y = (float)rand()/(float)RAND_MAX;
-        polygons[i].points[1].z = 0.0f;
+        polygons[i].points[1].z = (float)i/10.0f;
         polygons[i].points[2].x = (float)rand()/(float)RAND_MAX;
         polygons[i].points[2].y = (float)rand()/(float)RAND_MAX;
-        polygons[i].points[2].z = 0.0f;
+        polygons[i].points[2].z = (float)i/10.0f;
         polygons[i].points[3].x = (float)rand()/(float)RAND_MAX;
         polygons[i].points[3].y = (float)rand()/(float)RAND_MAX;
-        polygons[i].points[3].z = 0.0f;
+        polygons[i].points[3].z = (float)i/10.0f;
         polygons[i].color.r = (float)rand()/(float)RAND_MAX;
         polygons[i].color.g = (float)rand()/(float)RAND_MAX;
         polygons[i].color.b = (float)rand()/(float)RAND_MAX;
